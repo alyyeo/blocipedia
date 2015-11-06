d6a064a09574d9c1e8a2b1442196a4d3bf2d18fb
alyyeo <alyssalyyeo@gmail.com>
2015-11-06 06:21:29 +0000

    wikis can parse markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+require 'redcarpet'
 module ApplicationHelper
+    def markdown(text)
+        renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
+        markdown = Redcarpet::Markdown.new(renderer, 
+            autolink: true, 
+            space_after_headers: true,
+            highlight: true,
+            underline: true
+            )
+        markdown.render(text).html_safe
+    end
 end
